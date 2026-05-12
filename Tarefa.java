152d635822fa6f8ce357b999d0ccb5303677ccde
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-05-12 20:04:30 -0300

    Adicionando sistema de prioridade

--- Tarefa.java
+++ Tarefa.java
@@ -1,9 +1,11 @@
 public class Tarefa {
     String descricao;
     boolean concluida;
+    String prioridade; // NOVO ATRIBUTO
 
     public Tarefa(String descricao) {
         this.descricao = descricao;
         this.concluida = false;
+        this.prioridade = "Baixa"; // VALOR PADRÃO
     }
 }
