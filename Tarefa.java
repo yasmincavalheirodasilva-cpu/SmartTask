a46f46c54a9b0565ea89e045f023ea7165d273e6
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-05-12 21:24:16 -0300

    Data Entrega

--- Tarefa.java
+++ Tarefa.java
@@ -2,10 +2,12 @@ public class Tarefa {
     String descricao;
     boolean concluida;
     String prioridade; // NOVO ATRIBUTO
+    String dataEntrega;
 
-    public Tarefa(String descricao) {
+    public Tarefa(String descricao, String dataEntrega) {
         this.descricao = descricao;
         this.concluida = false;
+        this.dataEntrega = dataEntrega;
         this.prioridade = "Baixa"; // VALOR PADRÃO
     }
 
